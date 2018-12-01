42ac3526f5c1ad72b38f28a70584c5a9639bcc75
Ashwin <aswinjeganath@gmail.com>
2018-12-01 12:47:23 +0530

    Set up sass

--- solution/src/App.js
+++ solution/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
+import data from './mock_data';
+import mock_data from './mock_data';
 
 class App extends Component {
+  state = mock_data;
+
   render() {
-    return (
-      <div >
-        Hello
-      </div>
-    );
+    return <div>Hello World</div>;
   }
 }
 
